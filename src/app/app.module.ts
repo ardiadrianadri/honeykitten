bdb0563453920eb3b1748be9cf01f8e3b38e7c53
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2018-09-14 21:19:57 +0200

    Add the sing up form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -16,6 +17,7 @@ export const pages = [ HomePage, SignInPage, MyApp ];
   declarations: [...pages],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/sign_in/sign_in.ts
+++ src/pages/sign_in/sign_in.ts
@@ -1,7 +1,77 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, ValidationErrors, AbstractControl } from '@angular/forms';
 
 @Component({
     selector: 'sign-in',
     templateUrl: 'sign_in.html'
 })
-export class SignInPage {}
+export class SignInPage {
+
+    public singupForm: FormGroup;
+    public errorsMsg: {[key: string]: string } = {}
+
+    public get name() { return this.singupForm.get('name'); }
+    public get surname() { return this.singupForm.get('surname'); }
+    public get email() { return this.singupForm.get('email'); }
+    public get username() { return this.singupForm.get('username'); }
+    public get password() { return this.singupForm.get('password'); }
+    public get confirmation() { return this.singupForm.get('confirmation'); }
+    public get showErrors() {return Object.keys(this.errorsMsg).length > 0}
+    public get errors() {return Object.keys(this.errorsMsg); }
+
+    constructor(private _fb: FormBuilder) {
+        this.singupForm = this._fb.group({
+            name: ['', Validators.required],
+            surname: ['', Validators.required],
+            email: ['', Validators.compose([Validators.email, Validators.required])],
+            username: ['', Validators.required],
+            password: ['', Validators.required],
+            confirmation: ['', this._passwordValidation]
+        });
+    }
+
+   private _passwordValidation(control: AbstractControl): ValidationErrors | null {
+        let result = null;
+        let pwd: string;
+        let confir: string;
+
+        if (control.parent) {
+            pwd = control.parent.get('password').value;
+            confir = control.parent.get('confirmation').value;
+
+            result = pwd && pwd!==confir ? {'passwordNotMatch': true} : null;
+        }
+
+        return result;
+   }
+
+    public submit() {
+        console.log(this.singupForm);
+    }
+
+    public clean() {
+        console.log(this.singupForm);
+        console.log(this.errorsMsg);
+    }
+
+    public checkErrors (key: string) {
+        let formControl = this.singupForm.get(key);
+        if (formControl.errors) {
+            this.errorsMsg[key] = '';
+            Object.keys(formControl.errors).forEach((error:string) => {
+                switch(error) {
+                    case 'required':
+                        this.errorsMsg[key] += `The ${key} is required `;
+                    break;
+                    case 'email':
+                        this.errorsMsg[key] += `The ${key} has a wrong format `;
+                    break;
+                    default:
+                        this.errorsMsg[key] += `The passwords do not match`;
+                }
+            });
+        } else {
+            delete this.errorsMsg[key];
+        }
+    }
+}
